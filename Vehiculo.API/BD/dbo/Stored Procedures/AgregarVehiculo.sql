2e28ceea9ec0ed1a3a332a5b39f3bc2c4421850b
LuisCarlosQuirosObando <lquiros460787@ufide.ac.cr>
2026-03-09 20:54:31 -0600

    Copia de practica programada 3

---
+++ Vehiculo.API/BD/dbo/Stored Procedures/AgregarVehiculo.sql
@@ -0,0 +1,44 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE AgregarVehiculo
+	-- Add the parameters for the stored procedure here
+	@Id UNIQUEIDENTIFIER,
+    @IdModelo UNIQUEIDENTIFIER,
+    @Placa VARCHAR(MAX),
+    @Color VARCHAR(MAX),
+    @Anio INT,
+    @Precio DECIMAL(18,2),
+    @CorreoPropietario VARCHAR(MAX),
+    @TelefonoPropietario VARCHAR(MAX)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+    BEGIN TRANSACTION
+	INSERT INTO [dbo].[Vehiculo]
+           ([Id]
+           ,[IdModelo]
+           ,[Placa]
+           ,[Color]
+           ,[Anio]
+           ,[Precio]
+           ,[CorreoPropietario]
+           ,[TelefonoPropietario])
+     VALUES
+           (@Id,
+            @IdModelo,
+            @Placa,
+            @Color,
+            @Anio,
+            @Precio,
+            @CorreoPropietario,
+            @TelefonoPropietario)
+    SELECT @Id
+    COMMIT TRANSACTION
+END
---
+++ Vehiculo.API/BD/dbo/Stored Procedures/EditarVehiculo.sql
@@ -0,0 +1,36 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE EditarVehiculo
+	-- Add the parameters for the stored procedure here
+	@Id UNIQUEIDENTIFIER,
+    @IdModelo UNIQUEIDENTIFIER,
+    @Placa VARCHAR(MAX),
+    @Color VARCHAR(MAX),
+    @Anio INT,
+    @Precio DECIMAL(18,2),
+    @CorreoPropietario VARCHAR(MAX),
+    @TelefonoPropietario VARCHAR(MAX)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+    BEGIN TRANSACTION
+    UPDATE [dbo].[Vehiculo]
+       SET 
+           [IdModelo] = @IdModelo
+          ,[Placa] = @Placa
+          ,[Color] = @Color
+          ,[Anio] = @Anio
+          ,[Precio] = @Precio
+          ,[CorreoPropietario] = @CorreoPropietario
+          ,[TelefonoPropietario] = @TelefonoPropietario
+     WHERE Id=@Id
+     SELECT @Id
+     COMMIT TRANSACTION
+END
